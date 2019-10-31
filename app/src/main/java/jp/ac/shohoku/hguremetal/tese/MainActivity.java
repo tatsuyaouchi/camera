88d9577d0506180c1aec2be08f73eb29d249dee1
tatsuyaouchi <s18b703@shohoku.ac.jp>
2019-10-31 17:05:27 +0900

    クリックしたら音が出ますwww

--- app/src/main/java/jp/ac/shohoku/hguremetal/tese/MainActivity.java
+++ app/src/main/java/jp/ac/shohoku/hguremetal/tese/MainActivity.java
@@ -11,17 +11,28 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.os.Bundle;
+import android.media.AudioAttributes;
+import android.media.AudioManager;
+import android.media.SoundPool;
+import android.os.Build;
 
 
 public class MainActivity extends AppCompatActivity {
     private final static int RESULT_CAMERA = 1000;
     private ImageView imageView;
 
+    // サウンドの部分
+    SoundPool soundPool;
+    int mp3a;
+    int mp3b;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // カメラの部分
         imageView = findViewById(R.id.image_view);
 
         Button cameraButton = findViewById(R.id.camera_button);
@@ -30,10 +41,29 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v){
                 Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                 startActivityForResult(intent, RESULT_CAMERA);
+                // ボタン押すとサウンドがなるよ
+                soundPool.play(mp3a, 1f, 1f, 0, 0, 1f);
             }
         });
+
+        // サウンドの部分
+        if(Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP){
+            soundPool = new SoundPool(5, AudioManager.STREAM_MUSIC, 0);
+        }else{
+            AudioAttributes attr = new AudioAttributes.Builder()
+                    .setUsage(AudioAttributes.USAGE_MEDIA)
+                    .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                    .build();
+            soundPool = new SoundPool.Builder()
+                    .setAudioAttributes(attr)
+                    .setMaxStreams(5)
+                    .build();
+        }
+        mp3a = soundPool.load(this, R.raw.a, 1);
+        mp3b = soundPool.load(this, R.raw.a, 1);
     }
 
+    // カメラの部分
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data){
         if(requestCode == RESULT_CAMERA){
@@ -58,4 +88,9 @@ public class MainActivity extends AppCompatActivity {
 
         }
     }
+
+
+
+
+
 }
