0f9bc6713f373c83ee5b13c7f097f14eae232ed5
s18b702 <s18b702@shohhoku.ac.jp>
2019-11-14 16:19:40 +0900

    gamegamen

---
+++ app/src/main/java/jp/ac/shohoku/hguremetal/tese/SubActivity.java
@@ -0,0 +1,20 @@
+package jp.ac.shohoku.hguremetal.tese;
+//AndroididX
+//import android.supper.v7.app.CompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SubActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.homegamen);
+
+
+    }
+}
