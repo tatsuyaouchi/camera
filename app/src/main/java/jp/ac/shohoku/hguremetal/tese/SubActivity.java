628df8b81ffb706c5d7881203396fddd7fa6df4c
s18b702 <s18b702@shohhoku.ac.jp>
2019-11-28 16:34:16 +0900

    画面切り替えタイトルから遊び方

--- app/src/main/java/jp/ac/shohoku/hguremetal/tese/SubActivity.java
+++ app/src/main/java/jp/ac/shohoku/hguremetal/tese/SubActivity.java
@@ -12,8 +12,8 @@ public class SubActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.homegamen);
+
 
 
     }
